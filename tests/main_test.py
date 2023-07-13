307fca7077e9d49145a4fe8293c9d75c8794e221
Андрей Милютин <andrew.milyutin@gmail.com>
2023-07-13 20:18:34 +0300

    unittesting user service

---
+++ tests/main_test.py
@@ -0,0 +1,24 @@
+import unittest
+from main import app
+
+class MainTest(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    def test_create_user(self):
+        data = {
+            'username': 'test_user',
+            'email': 'test@example.com'
+        }
+        response = self.client.post('/users', json=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_user(self):
+        user_id = 1
+        response = self.client.get(f'/users/{user_id}')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
